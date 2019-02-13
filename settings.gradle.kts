59aba5cb4a092012328b866cfe853c8ca3f7b51d
Christopher <schwabchris95@googlemail.com>
2019-02-13 10:00:11 +0100

    Create new gradle project
    Using Kotlin DSL build script
    Additional Libraries and Frameworks: Java, Kotlin (Java)

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        jcenter()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.run { startsWith("kotlin") || startsWith("org.jetbrains.kotlin") }) {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20")
+                useVersion("1.3.20")
+            }
+        }
+    }
+}
+
+rootProject.name = "kotlin-mpp-mobile"
