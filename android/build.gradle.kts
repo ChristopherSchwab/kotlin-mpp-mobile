10cfed5c74900abfd415222fb79a5d0a04891ca8
Unknown <schwabchris95@googlemail.com>
2019-04-12 07:06:56 +0200

    [a] RecyclerView and ViewHolder for displaying the movie list

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 repositories {
     google()
     jcenter()
+    maven { setUrl("https://kotlin.bintray.com/kotlinx") }
     mavenCentral()
 }
 
@@ -39,6 +40,13 @@ dependencies {
     implementation(project(":common"))
     implementation("com.android.support:appcompat-v7:28.0.0")
     implementation("com.android.support.constraint:constraint-layout:1.1.3")
+    implementation("com.android.support:design:28.0.0")
+    implementation("com.android.support:recyclerview-v7:28.0.0")
+    implementation("com.android.support:cardview-v7:28.0.0")
+    implementation("io.ktor:ktor-client-json-jvm:1.0.0")
+    implementation("io.ktor:ktor-client-okhttp:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.0.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
     testImplementation("junit:junit:4.12")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
     androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
--- android/src/main/kotlin/com/example/MainActivity.kt
+++ android/src/main/kotlin/com/example/MainActivity.kt
@@ -2,11 +2,76 @@ package com.example
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.rvMovieList
+import common.tmdb.entities.MovieViewItem
+import common.tmdb.MovieListController
+import common.tmdb.MovieListInteractor
+import common.tmdb.MovieListPresenter
+import common.tmdb.MovieListView
+import common.util.HttpClientHttpRequestSerializer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : MovieListView, AppCompatActivity(), OnPageEndCallback, CoroutineScope {
+
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    val movieListController = MovieListController(
+        MovieListInteractor(
+            MovieListPresenter(this),
+            HttpClientHttpRequestSerializer(),
+            "api.themoviedb.org",
+            "0a055ad296b0a5d7496d9a0f0cb2a7b0"
+        )
+    )
+
+    private val movieListAdapter = MovieListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        job = Job()
+
+        val movieListLayoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false).apply {
+            spanSizeLookup = MovieListSpanSizeLookup(movieListAdapter)
+        }
+
+        val movieListScrollListener = MovieListScrollListener(movieListLayoutManager, 4, this)
+
+        rvMovieList.apply {
+            adapter = movieListAdapter
+            layoutManager = movieListLayoutManager
+            addOnScrollListener(movieListScrollListener)
+        }
+
+        movieListController.onCreate()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
+    override fun showMovieViewItems(movieViewItems: List<MovieViewItem>) {
+        launch {
+            movieListAdapter.addMovies(movieViewItems)
+        }
+    }
+
+    override fun onPageEnd() {
+        movieListController.onPageEnd()
+    }
+
+    override fun showError() {
+        Toast.makeText(this, R.string.error, Toast.LENGTH_LONG)
+    }
+
 }
